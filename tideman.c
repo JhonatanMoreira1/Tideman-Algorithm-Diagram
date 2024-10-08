189d730f6857471dc91c5487d9fb9c20a50f7aed
Jhonatan Moreira <jhonathanm836@gmail.com>
2024-10-08 19:39:25 -0300

    Official tideman archive with comments

--- tideman.c
+++ tideman.c
@@ -2,30 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
-// Número máximo de candidatos
+// Maximum number of candidates
 #define MAX 9
 
-// preferências[i][j] é o número de eleitores que preferem i a j
+// preferences[i][j] is the number of voters who prefer candidate i over candidate j
 int preferences[MAX][MAX];
 
-// bloqueado[i][j] significa que i está bloqueado em j
+// locked[i][j] means candidate i is locked in over candidate j
 bool locked[MAX][MAX];
 
-// Cada par tem um vencedor, um perdedor
+// Each pair has a winner and a loser
 typedef struct
 {
     int winner;
     int loser;
 } pair;
 
-// Matriz de candidatos
+// Array of candidates
 string candidates[MAX];
 pair pairs[MAX * (MAX - 1) / 2];
 
 int pair_count;
 int candidate_count;
 
-// Protótipos de funções
+// Function prototypes
 bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
 void add_pairs(void);
@@ -35,14 +35,14 @@ void print_winner(void);
 
 int main(int argc, string argv[])
 {
-    // Verifica se há uso inválido
+    // Check for invalid usage
     if (argc < 2)
     {
         printf("Usage: tideman [candidate ...]\n");
         return 1;
     }
 
-    // Preencher array de candidatos
+    // Populate array of candidates
     candidate_count = argc - 1;
     if (candidate_count > MAX)
     {
@@ -51,10 +51,10 @@ int main(int argc, string argv[])
     }
     for (int i = 0; i < candidate_count; i++)
     {
-        candidates[i] = argv[i + 1]; // Enche o array de candidatos com os nomes dos candidatos
+        candidates[i] = argv[i + 1]; // Fill the candidates array with candidate names
     }
 
-    // Limpa o gráfico de pares bloqueados
+    // Clear graph of locked pairs
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
@@ -66,13 +66,13 @@ int main(int argc, string argv[])
     pair_count = 0;
     int voter_count = get_int("Number of voters: ");
 
-    // Consulta por votos
+    // Query for votes
     for (int i = 0; i < voter_count; i++)
     {
-        // ranks[i] é a i-ésima preferência do eleitor
+        // ranks[i] is the ith preference for the current voter
         int ranks[candidate_count];
 
-        // Consulta para cada classificação
+        // Query for each rank
         for (int j = 0; j < candidate_count; j++)
         {
             string name = get_string("Rank %i: ", j + 1);
@@ -96,7 +96,7 @@ int main(int argc, string argv[])
     return 0;
 }
 
-// Atualiza as classificações com uma nova votação
+// Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
@@ -110,7 +110,7 @@ bool vote(int rank, string name, int ranks[])
     return false;
 }
 
-// Atualiza as preferências de acordo com a classificação de um eleitor
+// Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
@@ -123,7 +123,7 @@ void record_preferences(int ranks[])
     return;
 }
 
-// Registra pares de candidatos onde um é preferido ao outro
+// Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
     pair_count = 0;
@@ -142,10 +142,10 @@ void add_pairs(void)
     return;
 }
 
-// Classifica os pares em ordem decrescente pela força da vitória
+// Sort pairs in decreasing order by the strength of victory
 void sort_pairs(void)
 {
-    // Usar um algoritmo de ordenação, nesse caso, bubble sort
+    // Use a sorting algorithm, in this case, bubble sort
     for (int i = 0; i < pair_count - 1; i++)
     {
         for (int j = 0; j < pair_count - i - 1; j++)
@@ -154,7 +154,7 @@ void sort_pairs(void)
             int strength_j1 = preferences[pairs[j + 1].winner][pairs[j + 1].loser];
             if (strength_j1 > strength_j)
             {
-                // Trocar pares
+                // Swap pairs
                 pair temp = pairs[j];
                 pairs[j] = pairs[j + 1];
                 pairs[j + 1] = temp;
@@ -163,7 +163,7 @@ void sort_pairs(void)
     }
 }
 
-// Verifica se a adição de uma aresta(edge) cria um ciclo
+// Check if adding an edge creates a cycle
 bool has_cycle(int start, int candidate)
 {
     if (start == candidate)
@@ -180,7 +180,7 @@ bool has_cycle(int start, int candidate)
     return false;
 }
 
-// Bloqueia os pares no gráfico candidato em ordem, sem criar ciclos
+// Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
@@ -192,7 +192,7 @@ void lock_pairs(void)
     }
 }
 
-// Imprime o vencedor da eleição
+// Print the winner of the election
 void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
